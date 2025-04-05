b17894736f0ee1e038aa90c73c9ec763c65fb0f8
GauriManda <145831817+GauriManda@users.noreply.github.com>
2025-04-05 16:01:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "./Components/Header";
+//import QuoteBox from './Components/QuoteBox';
+import LandingPage from './Components/LandingPage';
+
+function App() {
+  return (
+    <div className="app-container">
+     <Header/>
+     
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
